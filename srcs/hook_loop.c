987e3faec0278663c3002d140b2d5871b5a9db86
Mickael DOS SANTOS <mdos-san@e2r9p18.42.fr>
2016-02-19 10:39:05 +0100

    norm

--- srcs/hook_loop.c
+++ srcs/hook_loop.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 13:45:46 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/02/18 16:17:03 by ahamouda         ###   ########.fr       */
+/*   Updated: 2016/02/19 10:38:36 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,14 @@
 
 int	hook_loop(t_env *env)
 {
-	(env->player.movement_y == -1) ? wolf3d_player_move(env, 0.1 * env->player.view_dir.x, 0.1 * env->player.view_dir.y) : 0;
-	(env->player.movement_y == 1) ? wolf3d_player_move(env, -0.1 * env->player.view_dir.x, -0.1 * env->player.view_dir.y) : 0;
-	(env->player.movement_x == 1) ? wolf3d_player_move(env, -0.1 * env->player.view_dir.y, 0.1 * env->player.view_dir.x) : 0;
-	(env->player.movement_x == -1) ? wolf3d_player_move(env, 0.1 * env->player.view_dir.y, -0.1 * env->player.view_dir.x) : 0;
+	(env->player.movement_y == -1) ? wolf3d_player_move
+(env, 0.1 * env->player.view_dir.x, 0.1 * env->player.view_dir.y) : 0;
+	(env->player.movement_y == 1) ? wolf3d_player_move
+(env, -0.1 * env->player.view_dir.x, -0.1 * env->player.view_dir.y) : 0;
+	(env->player.movement_x == 1) ? wolf3d_player_move
+(env, -0.1 * env->player.view_dir.y, 0.1 * env->player.view_dir.x) : 0;
+	(env->player.movement_x == -1) ? wolf3d_player_move
+(env, 0.1 * env->player.view_dir.y, -0.1 * env->player.view_dir.x) : 0;
 	(env->player.view_move == 1) ? vec_rotate_2d(&env->player.view_dir, -2) : 0;
 	(env->player.view_move == -1) ? vec_rotate_2d(&env->player.view_dir, 2) : 0;
 	hook_expose(env);
--- srcs/wolf3d_player_move.c
+++ srcs/wolf3d_player_move.c
@@ -6,7 +6,7 @@
 /*   By: ahamouda <ahamouda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 16:07:43 by ahamouda          #+#    #+#             */
-/*   Updated: 2016/02/19 10:30:09 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/02/19 10:36:43 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void	wolf3d_player_move(t_env *env, double x, double y)
 {
-	if (env->map.map[(int)(env->player.pos.y + y)][(int)(env->player.pos.x + x)] == '0' ||
-	env->map.map[(int)(env->player.pos.y + y)][(int)(env->player.pos.x + x)] == '#')
+	if (env->map.map[(int)(env->player.pos.y + y)]
+					[(int)(env->player.pos.x + x)] == '0' ||
+		env->map.map[(int)(env->player.pos.y + y)]
+					[(int)(env->player.pos.x + x)] == '#')
 	{
 		env->player.pos.x += x;
 		env->player.pos.y += y;
