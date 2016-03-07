69761dbc3070f0f86722be89263ee15645c30a1a
Mickael DOS SANTOS <mdos-san@e1r6p7.42.fr>
2016-03-07 01:10:12 +0100

    collision with digit in 2d map

--- srcs/wolf3d_map_is_wall.c
+++ srcs/wolf3d_map_is_wall.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 17:45:24 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/02 17:54:30 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/07 01:08:31 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 int	wolf3d_map_is_wall(t_env *env, t_2d_pnt a)
 {
-	if (env->map.map[(int)(a.y / BLOCK)][(int)(a.x / BLOCK)] == '1')
-		return (1);
-	if (env->map.map[(int)(a.y / BLOCK)][(int)(a.x / BLOCK)] == '\0')
+	if (ft_isdigit(env->map.map[(int)(a.y / BLOCK)][(int)(a.x / BLOCK)]))
 		return (1);
 	return (0);
 }
