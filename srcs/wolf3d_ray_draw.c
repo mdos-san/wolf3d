f24d00994e188a963d83c383e6e76256bfdb6857
Mickael DOS SANTOS <mdos-san@e2r2p8.42.fr>
2016-03-10 10:49:58 +0100

    first try

--- srcs/wolf3d_ray_draw.c
+++ srcs/wolf3d_ray_draw.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 11:19:21 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/10 09:11:25 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/10 10:47:25 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,21 @@
 **
 */
 
+static double	get_dist(t_2d_pnt a, t_2d_pnt b)
+{
+	return (sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)));
+}
+
 static double	next_x(t_2d_pnt pnt, char dir)
 {
 	if (dir == -1)
 	{
-		pnt.x -= 0.00001;
 		pnt.x /= BLOCK;
 		pnt.x = (int)pnt.x;
 		pnt.x *= BLOCK;
-		return ((int)pnt.x);
+		if ((int)pnt.x == 0)
+			return (0);
+		return ((int)pnt.x - 0.0001);
 	}
 	if (dir == 1)
 	{
@@ -50,11 +56,12 @@ static double	next_y(t_2d_pnt pnt, char dir)
 {
 	if (dir == -1)
 	{
-		pnt.y -= 0.00001;
 		pnt.y /= BLOCK;
 		pnt.y = (int)pnt.y;
 		pnt.y *= BLOCK;
-		return ((int)pnt.y);
+		if ((int)pnt.y == 0)
+			return (0);
+		return ((int)pnt.y - 0.0001);
 	}
 	if (dir == 1)
 	{
@@ -92,21 +99,32 @@ void			wolf3d_ray_draw(t_env *env, t_2d_ray *ray, unsigned int color,
 		{
 			pnt.x = (ray->dir.x > 0) ? next_x(pnt, 1) : next_x(pnt, -1);
 			pnt.y = m * pnt.x + p;
-//			printf("pnt x %lf, y %lf\n", pnt.x, pnt.y);
-//			if (0 <= pnt.y && pnt.y < env->map.size_y && (0 <= pnt.x && pnt.x <= ft_strlen(env->map.map[(int)(pnt.y / BLOCK)]) * BLOCK))
-//			{
-//				if(wolf3d_map_is_wall(env, pnt))
-//					condition++;
-//			}
+//			printf("pnt x %lf, y %lf, m %lf\n", pnt.x, pnt.y, m);
+			if (0 <= pnt.y && pnt.y < env->map.size_y && (0 <= pnt.x && pnt.x <= ft_strlen(env->map.map[(int)(pnt.y / BLOCK)]) * BLOCK))
+			{
+				if(wolf3d_map_is_wall(env, pnt))
+				{
+//					printf("pnt x %lf, y %lf, m %lf\n", pnt.x, pnt.y, m);
+//					img_putline(env, ray->o, pnt, 0xfffffff);
+					condition++;
+				}
+			}
 			pnt2.y = (ray->dir.y > 0) ? next_y(pnt2, 1) : next_y(pnt2, -1);
 			pnt2.x = (pnt2.y - p) / m;
-			printf("pnt2 x %lf, y %lf, m %lf\n", pnt.x, pnt.y, m);
+//			printf("pnt2 x %lf, y %lf, m %lf\n", pnt2.x, pnt2.y, m);
 			if (0 <= pnt2.y && pnt2.y < env->map.size_y && (0 <= pnt2.x && pnt2.x <= ft_strlen(env->map.map[(int)(pnt2.y / BLOCK)]) * BLOCK))
 			{
 				if(wolf3d_map_is_wall(env, pnt2))
+				{
+//					printf("pnt2 x %lf, y %lf, m %lf\n", pnt2.x, pnt2.y, m);
+//					img_putline(env, ray->o, pnt2, 0xfffff);
 					condition++;
+				}
 			}
+//			condition++;
 		}
+		pnt = (get_dist(ray->o, pnt) < get_dist(ray->o, pnt2)) ? pnt : pnt2;
+		img_putline(env, ray->o, pnt, 0xff00ff);
 	}
 	(void)env;
 	(void)ray;
