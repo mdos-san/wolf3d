e9a74dea2bf692f7cb81cee0dac6a694fd76c78b
Mickael DOS SANTOS <mdos-san@e2r6p10.42.fr>
2016-03-17 14:05:08 +0100

    Changing a little bit textures load function to all textures with one function

--- srcs/textures_load.c
+++ srcs/textures_load.c
@@ -6,19 +6,20 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/17 13:24:05 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/17 13:30:36 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/17 14:03:26 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-int	textures_load(t_env *env)
+int	textures_load(t_env *env, int nbr, char *path)
 {
-	env->textures[0].ptr = mlx_xpm_file_to_image(env->mlx,
-		"textures/wall_1.xpm",
-		&env->textures[0].width, &env->textures[0].height);
-	env->textures[0].data = mlx_get_data_addr(env->textures[0].ptr,
-			&env->textures[0].bpp, &env->textures[0].sl, &env->textures[0].ed);
-	env->textures[0].bpp /= 8;
+	env->textures[nbr].ptr = mlx_xpm_file_to_image(env->mlx,
+		path,
+		&env->textures[nbr].width, &env->textures[nbr].height);
+	env->textures[nbr].data = mlx_get_data_addr(env->textures[nbr].ptr,
+			&env->textures[nbr].bpp, &env->textures[nbr].sl,
+			&env->textures[nbr].ed);
+	env->textures[nbr].bpp /= 8;
 	return (1);
 }
